b594e51fe09ffe4314110b975459c03367ab8b5d
statiolake <statiolake@gmail.com>
2026-04-05 18:27:45 +0000

    feat: デフォルトの通知スタイルを利用する

--- ecscd/src/app/layout.tsx
+++ ecscd/src/app/layout.tsx
@@ -33,7 +33,13 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
-        <Toaster position="bottom-right" duration={10000} richColors closeButton />
+        <Toaster
+          position="top-right"
+          duration={10000}
+          richColors
+          closeButton
+          visibleToasts={5}
+        />
       </body>
     </html>
   );
